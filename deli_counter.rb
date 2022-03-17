36521f76d0f018bfbb83a39581d2d86f2792257e
+ <+>
2022-03-17 14:56:31 -0400

    commit tree e0f39bccec27955f9079cce6be971896b00c8b5b

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,23 @@
 # Write your code here.
 
-
 def line(katz_deli)
-    if katz_deli.length != 0
-        line = ["The line is currently:"]
-        katz_deli.map.with_index{|person, index| line.push("#{index + 1}. #{person}")}
-        puts line.join(" ")
-    else puts "The line is currently empty."
-    end
-end 
+  if katz_deli.length != 0
+    line = "The line is currently:"
+    katz_deli.each.with_index(1) { |person, index| line << (" #{index}. #{person}") }
+    puts line
+  else puts "The line is currently empty."   end
+end
 
 def take_a_number(katz_deli, new_person)
-    katz_deli << new_person
-    if katz_deli.length > 0
-        puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
-    else puts "The line is empty."
-    end
-end 
+  katz_deli << new_person
+  puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
+end
 
 def now_serving(katz_deli)
-    if katz_deli.length == 0 
-        puts "There is nobody waiting to be served!"
-    else 
-        puts "Currently serving #{katz_deli.first}."
-        katz_deli.shift
-    end
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
